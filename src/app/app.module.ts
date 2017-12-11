23513cf0005069b9a3163d69b3681fce03e4ae5d
joseph.joshy <joseph.joshy@mu-sigma.com>
2017-12-11 21:11:51 +0530

    Setting Base structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
+// Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule} from '@angular/http';
+import { ToasterModule, ToasterService } from 'angular2-toaster';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ModulesModule } from './modules/modules.module';
+import { ServicesModule } from './services/services.module';
+// Components
 import { AppComponent } from './app.component';
-
+import { AppRoutingModule } from './/app-routing.module';
+import { Ng4LoadingSpinnerModule } from 'ng4-loading-spinner';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpModule,
+    ToasterModule,
+    ModulesModule,
+    ServicesModule,
+    BrowserAnimationsModule,
+    Ng4LoadingSpinnerModule.forRoot(),
+
   ],
-  providers: [],
+  providers: [ToasterService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
